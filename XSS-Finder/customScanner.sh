478d9c352d5655e7d22531430b78c9800f5fa86a
tomsqr94 <tom_sqr@hotmail.co.uk>
2018-01-31 19:12:56 +0000

    Update customScanner.sh
    Updated this to remove false python syntax

--- XSS-Finder/customScanner.sh
+++ XSS-Finder/customScanner.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-## Customer Scanner v1.0 - T.S 31/01/18
-## This script merges DorkScanner + XSScrapy together to quickly scan sites instantly.
+## Customer Scanner v1.0
+## This allows you to enter the Dork you wish to run, and number of pages to run.
+## It then automatically starts scanning all urls obtained through XSSCRAPY.py
+## Run this from the XSScrapy Dir!
 
-dork=raw_input("Please enter the Google Dork you wish to use: ")
-num=raw_input("How many google pages to sieve through?: ")
+echo "Please enter the Google Dork you wish to use: "
+read dork
+echo "How many google pages to sieve through?: "
+read num
 
 echo "Running Dork Scan..."
-
 python3 /opt/DorkScan/scanner.py $dork $num > /opt/xsscrapy/urls.txt
 
 echo "Scan completed and outputted to urls.txt in /opt/xsscrapy/ Directory"
 
 echo "Scanning the list of URL's with xsscrapy now"
-for i in $(cat urls.txt); do python /opt/xsscrapy/xsscrapy.py -u $i; done
+for i in $(cat /opt/xsscrapy/urls.txt); do python xsscrapy.py -u $i; done
